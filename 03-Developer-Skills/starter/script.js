009cd48024cb772dbbf3894aef12eaf455214e27
Stephane <stephane@srstudio.co.uk>
2024-04-02 11:42:30 +0100

    My first lines in JavaScript

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const forecastTemps = [17, 21, 23];
+const forecastTemps2 = [12, 5, -5, 0, 4];
+
+function printForecast(arr) {
+  let printStr = '...';
+
+  for (let i = 0; i < arr.length; i++) {
+    printStr += ` ${arr[i]}C in ${i + 1} days ...`;
+  }
+
+  console.log(printStr);
+}
+
+printForecast(forecastTemps2);
